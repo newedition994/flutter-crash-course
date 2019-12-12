9fd7ed815e0ec39c3c0931fd8673ab6e27eaa282
Breyanna Vass <breyannavass@gmail.com>
2019-12-11 23:23:28 -0500

    adding in the heart icons for favorites

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -28,11 +28,16 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+
     return ListTile(
-        title: Text(
-      pair.asPascalCase,
-      style: TextStyle(fontSize: 18.0),
-    ));
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.pink[100] : null),
+    );
   }
 
   Widget build(BuildContext context) {
