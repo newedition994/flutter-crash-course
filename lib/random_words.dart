75e1648fff816ab97bdac3ec8c145b398e8d46e6
Breyanna Vass <breyannavass@gmail.com>
2019-12-11 23:39:00 -0500

    made the route viewable after being saved

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -49,7 +49,26 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-  void _pushedSaved() {}
+  void _pushedSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+          title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)),
+        );
+      });
+
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved WordPairs'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
