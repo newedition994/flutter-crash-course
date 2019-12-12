32b16eebd2747f7b2a1fb05eff11ff33214dc340
Breyanna Vass <breyannavass@gmail.com>
2019-12-11 23:03:25 -0500

    made the list view possible

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,31 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  Widget _buildList() {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[500],
+          child: const Center(child: Text('Entry B')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[100],
+          child: const Center(child: Text('Entry C')),
+        ),
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('WordPair Generator')),
-      body: Text('Hello World'),
-    );
+        appBar: AppBar(title: Text('WordPair Generator')), body: _buildList());
   }
 }
